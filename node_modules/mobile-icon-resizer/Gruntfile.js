0e36cf1093ea3372b1f7c08692bdc2d7a7997143
astray <166081994@qq.com>
2018-05-02 21:11:22 +0800

    remove node modules

--- node_modules/mobile-icon-resizer/Gruntfile.js
+++
@@ -1,7 +0,0 @@
-var gruntCommons = require('muzzley-grunt-commons');
-
-module.exports = function (grunt) {
-  var commonsConfig = gruntCommons.getInitConfig(grunt);
-  grunt.initConfig(commonsConfig);
-  gruntCommons.setup(grunt);
-};
