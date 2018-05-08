68a38a1c26e058a2668c8f111b31dfdb3228faaa
astray <166081994@qq.com>
2018-05-08 15:22:53 +0800

    server db changed

--- src/config/server_db_config.ts
+++ src/config/server_db_config.ts
@@ -2,8 +2,8 @@ export default {
     type: "mysql",
     host: "localhost",
     port: 3306,
-    username: "nong",
-    password: "hzx123",
+    username: "root",
+    password: "kmcx110",
     database: "app_manage",
     entities: [
         __dirname + "/entity/*.js"
