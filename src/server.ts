ac45a97a5e81f220594a21e76f26090c042ed711
astray <166081994@qq.com>
2018-05-08 15:47:35 +0800

    dbconfig fix

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,62 @@
 import { app } from './app';
 import "reflect-metadata";
-import { createConnection } from "typeorm";
+import { getConnectionManager, ConnectionManager, Connection,createConnection } from "typeorm";
 // import {Login} from './entity/login';
 import * as path from 'path';
 import local_db_config from './config/local_db_config';
 import server_db_config from './config/server_db_config';
 
-let db_config;
+let db_config: db_config
+
+interface db_config {
+    type: string,
+    host: string,
+    port: number,
+    username: string,
+    password: string,
+    database: string,
+    entities: any,
+    synchronize: boolean
+}
 
 console.log('环境变量', process.env.NODE_ENV);
-switch(process.env.NODE_ENV){
-    case 'dev':
-        db_config = local_db_config;
-        break;
-    case 'production':
-        db_config = server_db_config;
-        break;
+// switch(process.env.NODE_ENV){
+//     case 'dev':
+//         db_config = local_db_config;
+//         break;
+//     case 'production':
+//         db_config = server_db_config;
+//         break;
+// }
+if(process.env.NODE_ENV === 'dev'){
+    db_config = local_db_config;
+}else{
+    db_config = server_db_config;
 }
 console.log(db_config);
 
 // console.log( '../' + __dirname + "/entity/*.ts")
+
+// (async() =>{
+//     // const connection = await createConnection();
+//     const connectionManager = new ConnectionManager();
+//     const connection = connectionManager.create(db_config);
+//     await connection.connect();
+//     const server = app.listen(7890, () => {
+//         console.log('Server is running at http://localhost:7890');
+//         console.log('Press CTRL-C to stop \n');
+//     });
+// })
+
+
+
 createConnection({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "nong",
-    password: "hzx123",
-    database: "app_manage",
+    host: db_config.host,
+    port: db_config.port,
+    username: db_config.username,
+    password: db_config.password,
+    database: db_config.database,
     entities: [
         __dirname + "/entity/*.js"
 
